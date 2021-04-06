887b30572911b12aa42ec90a6dfc41c914132101
Enzo Piccoli <piccolienzo@hotmail.com>
2021-04-05 23:03:50 -0300

    2nd commit

--- TP1.2-Simulacion.py
+++ TP1.2-Simulacion.py
@@ -9,62 +9,113 @@ import matplotlib.patches as mpatches
 
 
 
-def main():
+def main():  
     
-    capital_jugador = int(valid_input(0,50,"Con cuanto inicial dinero desea jugar? ","Error: Ingrese una opcion valida "))
+    global capital_jugador
+    global contador 
+    global capital_caja
+
+    
+    capital_jugador = int(valid_input(0,10000,"Con cuanto inicial dinero desea jugar? ","Error: Ingrese una opcion valida "))
+    estrategia = int(valid_input(1,2,"Que estrategia desea jugar 1- D'Alembert 2-Martin Gala ","Error: Ingrese una estrategia valida "))
+    callback = 0
+    if(estrategia == 1):
+        callback = d_alembert
+    else:
+        callback = martin_gala
+
     ficha = valid_input(1,5,"Que ficha desea jugar?: "+ str(fichas), "Error: Ingrese valor de ficha valido")
+    
     ficha = fichas[ficha]
     
     
     jugada = valid_input(1,7,"Que tipo de jugada realizará?\n1. Pleno\n2. Docenas\n3. Filas\n4. Rojo\n5. Negro\n6. Par\n7. Impar","Error: Seleccione jugada válida")
 
-    capital_jugador-=ficha
-
+    
     if jugada == 1:
         apuesta = valid_input(0,36,"Ingrese jugada al pleno","Error: Ingrese una opcion valida de ruleta")
-        jugar(ficha,[apuesta],36,capital_jugador)
+       
+        jugar(ficha, ficha, [apuesta], 36, callback)
     
     elif jugada == 2:
         apuesta = valid_input(1,3,"Ingrese docena a apostar [1,2,3]","Error: Ingrese una opcion valida de ruleta")
-        jugar(ficha,[docenas[apuesta]],3,capital_jugador)
+        
+        jugar(ficha, ficha, [docenas[apuesta]], 3, callback)
     
     elif jugada == 3:
-        apuesta = valid_input(1,3,"Ingrese filas a apostar [1,2,3]","Error: Ingrese una opcion valida de ruleta")
-        jugar(ficha,[filas[apuesta]],3,capital_jugador)
+        apuesta = valid_input(1,3,"Ingrese filas a apostar [1,2,3]","Error: Ingrese una opcion valida de ruleta")     
+        jugar(ficha, ficha, [filas[apuesta]], 3, callback)
     
     elif jugada == 4:
         print("Usted aposto a ROJO")
-        jugar(ficha,rojo,2,capital_jugador)
+        jugar(ficha, ficha, rojos, 2, callback)
 
     elif jugada == 5:
         print("Usted aposto a NEGRO")
-        jugar(ficha,negro,2,capital_jugador)
+        jugar(ficha, ficha, negros, 2, callback)
 
     elif jugada == 6:
         print("Usted aposto a PAR")
-        jugar(ficha,pares,2,capital_jugador)
+        
+        jugar(ficha, ficha, pares, 2, callback)
 
     elif jugada == 7:
         print("Usted aposto a IMPAR")
-        jugar(ficha,impares,2,capital_jugador)
+        jugar(ficha, ficha, impares, 2, callback)
 
     else:
         print('error')
+    
 
 
-def jugar(_ficha, _apuesta, _multiplicador,capital_jugador):      
+def d_alembert(_ficha,_apuesta):
+    var = _apuesta + _ficha   
+    return var
+
+def martin_gala(_ficha,apuesta):
+    var = apuesta * 2
+    return var
+
+
+def jugar(_apostado,_ficha, _apuesta, _multiplicador, estrategia): 
+    global capital_jugador
+    global contador
+    global capital_caja
+    #Ficha: valor de la ficha:5 #Apuesta: numeros donde debe salir:[5,40,30] #capital: 500 
+    
     nro_ruleta = getRandomInt()
+    print("capital caja antes de jugar: $"+str(capital_caja))
     gano = False
-    if nro_ruleta in _apuesta:
-        print("felicidades pa ganaste $"+ str(_ficha * _multiplicador))
-        print("saldo actual: $"+str(capital_jugador + _ficha * _multiplicador))
+    
+    if (capital_jugador >= _apostado and capital_caja >= _apostado):
+        #puede jugar
+        print("en juego hay $"+str(_apostado))
+        contador += 1
+        capital_jugador -=  _apostado
+        capital_caja += _apostado
+        print("saldo actual:"+str(capital_jugador))
+        if nro_ruleta in _apuesta:
+            #ganó
+            ganancia = _apostado * _multiplicador
+            capital_jugador += ganancia
+            capital_caja-=ganancia
+            #suponemos que no cambia la apuesta
+            print("gano: $"+str(ganancia)+" capital actual: $"+str(capital_jugador))
+            jugar(_ficha,_ficha,_apuesta,_multiplicador,estrategia)
+
+        else:
+            #perdio            
+            print("perdio: $"+str(_apostado)+" capital actual: $"+str(capital_jugador))
+            jugar(estrategia(_ficha,_apostado),_ficha,_apuesta,_multiplicador,estrategia)
+
 
     else:
-        print("perdiste paaaaaa -$"+str(_ficha))
-        print("saldo actual: $"+str(capital_jugador))   
-    
+        #no puede juegar mas
+        print("capital final: " + str(capital_jugador))
+        print("perdiste en "+ str(contador)+ " veces paaaaa")
+        print("capital caja: $"+str(capital_caja))
+
     return gano
-   
 
  
 def valid_input(min,max,message,error_message):
@@ -79,9 +130,6 @@ def valid_input(min,max,message,error_message):
             print(error_message)  
     return player_input
 
-
-
-
 #Retorna un entero aleatorio entre el minimo y el máximo
 def getRandomInt():
     number = random.randint(MIN,MAX)
@@ -94,14 +142,15 @@ MAX=36
 plenos =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
 docenas = [[0],[1,2,3,4,5,6,7,8,9,10,11,12],[13,14,15,16,17,18,19,20,21,22,23,24],[25,26,27,28,29,30,31,32,33,34,35,36]]
 filas = [[0],[1,4,7,10,13,16,19,22,25,28,31,34],[2,5,8,11,14,17,20,23,26,29,32,25],[3,6,9,12,15,18,21,24,27,30,33,36]]
-rojo = [1,3,5,7,9,12,14,16,18,21,23,25,27,30,32,34,36]
-negro = [2,4,6,8,10,11,13,15,17,19,20,22,24,26,28,29,31,33,35]
+rojos = [1,3,5,7,9,12,14,16,18,21,23,25,27,30,32,34,36]
+negros = [2,4,6,8,10,11,13,15,17,19,20,22,24,26,28,29,31,33,35]
 pares = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36]
 impares = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]
 
 
 fichas = {1:5,2:10,3:25,4:50,5:100}
 
-
-capital_caja = 100000
+contador = 0
+capital_caja = 1000000
+capital_jugador = 0
 main()
